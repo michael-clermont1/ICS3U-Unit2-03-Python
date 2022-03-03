d20fc32dfc2283cd24a8b784f95cdfa4820ef8f0
Michael Clermont <michael.clermont@mths.ca>
2022-03-03 18:48:18 +0000

    added circumference program with constants

---
+++ circumference.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Clermont
+# Created on: Feb 2022
+# This program calculates the circumference of a circle
+#    with radius inputted by the user and tau
+
+import constants
+
+
+def main():
+    # this function calculates the circumference
+
+    # input
+    radius = int(input("Enter the radius of the circle (mm): "))
+
+    # process
+    circumference = radius * constants.TAU
+
+    # output
+    print("")
+    print("Circumference is {0} mm².".format(circumference))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
